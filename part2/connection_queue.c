50b096b4d00a00e353b9cc36b9c5b2c2b1e54aae
Thien Nguyen <nguy3856@umn.edu>
2022-04-26 19:04:45 -0500

    wrote conxn_queue and queue.c

--- part2/connection_queue.c
+++ part2/connection_queue.c
@@ -1,28 +1,138 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "connection_queue.h"
 
 int connection_queue_init(connection_queue_t *queue) {
     // TODO Not yet implemented
+    // allocate memory
+    // initialize syncrhonization primitives
+    // mutexes and condition variables
+    memset(queue->client_fds, 0, sizeof(int)*CAPACITY);
+    queue->length = 0;
+    queue->read_idx = 0; // not sure what these two idx do
+    queue->write_idx = 0;
+    queue->shutdown = 1; // change to 0 for shutdown
+    int result;
+    if ((result = pthread_mutex_init(&queue->lock, NULL)) != 0) {
+        fprintf(stderr, "pthread_mutex_init: %s\n", strerror(result));
+        return -1;
+    }
+    if ((result = pthread_cond_init(&queue->queue_full, NULL)) != 0) {
+        fprintf(stderr, "pthread_cond_init: %s\n", strerror(result));
+        return -1;
+    }
+    if ((result = pthread_cond_init(&queue->queue_empty, NULL)) != 0) {
+        fprintf(stderr, "pthread_cond_init: %s\n", strerror(result));
+        return -1;
+    }
     return 0;
 }
 
 int connection_enqueue(connection_queue_t *queue, int connection_fd) {
     // TODO Not yet implemented
+    int result;
+    if ((result = pthread_mutex_lock(&queue->lock)) != 0) {
+        fprintf(stderr, "pthread_mutex_lock: %s\n", strerror(result));
+        return -1;
+    }
+    while (queue->length == CAPACITY) {
+        if ((result = pthread_cond_wait(&queue->queue_full, &queue->lock)) != 0) {
+            fprintf(stderr, "pthread_cond_wait: %s\n", strerror(result));
+            return -1;
+        }
+    }
+
+    if (queue->shutdown == 0) {
+        if ((result = pthread_mutex_unlock(&queue->lock)) != 0) {
+            fprintf(stderr, "pthread_mutex_unlock: %s\n", strerror(result));
+            return -1;
+        }
+        return -1;
+    } else {
+        queue->client_fds[queue->length] = connection_fd;
+        queue->length++;
+        if ((result = pthread_cond_signal(&queue->queue_empty)) != 0) {
+            fprintf(stderr, "pthread_cond_signal: %s\n", strerror(result));
+            pthread_mutex_unlock(&queue->lock);
+            return -1;
+        }
+    }
+
+    if ((result = pthread_mutex_unlock(&queue->lock)) != 0) {
+        fprintf(stderr, "pthread_mutex_unlock: %s\n", strerror(result));
+        return -1;
+    }
+
     return 0;
 }
 
 int connection_dequeue(connection_queue_t *queue) {
-    // TODO Not yet implemented
+    int result;
+    if ((result = pthread_mutex_lock(&queue->lock)) != 0) {
+        fprintf(stderr, "pthread_mutex_lock: %s\n", strerror(result));
+        return -1;
+    }
+    while (queue->length == 0) {
+        if ((result = pthread_cond_wait(&queue->queue_empty, &queue->lock)) != 0) {
+            fprintf(stderr, "pthread_cond_wait: %s\n", strerror(result));
+            return -1;
+        }
+    }
+
+    if (queue->shutdown == 0) {
+        if ((result = pthread_mutex_unlock(&queue->lock)) != 0) {
+            fprintf(stderr, "pthread_mutex_unlock: %s\n", strerror(result));
+            return -1;
+        }
+        return -1;
+    } else {
+        if ((result = pthread_cond_signal(&queue->queue_empty)) != 0) {
+            fprintf(stderr, "pthread_cond_signal: %s\n", strerror(result));
+            pthread_mutex_unlock(&queue->lock);
+            return -1;
+        }
+        if ((result = pthread_mutex_unlock(&queue->lock)) != 0) {
+            fprintf(stderr, "pthread_mutex_unlock: %s\n", strerror(result));
+            return -1;
+        }
+        return queue->client_fds[queue->length--];
+    }    
     return 0;
 }
 
 int connection_queue_shutdown(connection_queue_t *queue) {
-    // TODO Not yet implemented
+    int result;
+    if ((result = pthread_mutex_lock(&queue->lock)) != 0) {
+        fprintf(stderr, "pthread_mutex_lock: %s\n", strerror(result));
+        return -1;
+    }
+
+    if ((result = pthread_cond_broadcast(&queue->queue_empty)) != 0) {
+        fprintf(stderr, "pthread_cond_signal: %s\n", strerror(result));
+        pthread_mutex_unlock(&queue->lock);
+        return -1;
+    }
+    queue->shutdown = 0;
+    if ((result = pthread_mutex_unlock(&queue->lock)) != 0) {
+        fprintf(stderr, "pthread_mutex_unlock: %s\n", strerror(result));
+        return -1;
+    }
+       
     return 0;
 }
 
 int connection_queue_free(connection_queue_t *queue) {
-    // TODO Not yet implemented
+    int result;
+    if ((result = pthread_mutex_lock(&queue->lock)) != 0) {
+        fprintf(stderr, "pthread_mutex_lock: %s\n", strerror(result));
+        return -1;
+    }
+    free(queue);
+    if ((result = pthread_mutex_unlock(&queue->lock)) != 0) {
+        fprintf(stderr, "pthread_mutex_unlock: %s\n", strerror(result));
+        return -1;
+    }
+        
     return 0;
 }
--- part2/http.c
+++ part2/http.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <unistd.h>
 #include "http.h"
+#include <stdlib.h>
 
 #define BUFSIZE 512
 
@@ -26,11 +27,68 @@ const char *get_mime_type(const char *file_extension) {
 }
 
 int read_http_request(int fd, char *resource_name) {
-    // TODO Not yet implemented
+
+    // copied from part 1
+    char buf[BUFSIZE];
+    bzero(buf, sizeof(buf));
+    if(read(fd, buf, BUFSIZE) < 0){
+        fprintf(stderr, "failed to read");
+        return 1;
+    }
+
+    char *str = strtok(buf, " ");
+    str = strtok(NULL, " ");//geting the file path
+    if(str == NULL)return 1;
+    strcpy(resource_name, str);
+    // end copied section
+
     return 0;
 }
 
 int write_http_response(int fd, const char *resource_path) {
-    // TODO Not yet implemented
+    
+    // copied from part 1
+    char response[BUFSIZE];
+    bzero(response, sizeof(response));
+    struct stat *buf;
+    buf  = malloc(sizeof(struct stat));
+    char *error_file = "server_files/error.html";
+    int file;
+    if(stat(resource_path, buf) == -1){
+        errno = ENOENT;
+        strcpy(response, "HTTP/1.1 404 NOT FOUND\r\n\n");
+        file = open(error_file, O_RDONLY);
+
+    }else{
+        char *f_extension = strchr(resource_path, '.');
+        file = open(resource_path, O_RDONLY);
+        sprintf(response,"HTTP/1.0 200 OK\r\nContent-Type: %s\nContent-Length: %ld\r\n\r\n",get_mime_type(f_extension), buf->st_size);
+    }
+    free(buf);
+    fprintf(stderr, "%s", response);
+    if(write(fd, response, strlen(response)) < 0){
+        fprintf(stderr, "failed to write reponse to client");
+        return 1;
+    }
+    
+    if(file < 0){
+        fprintf(stderr, "failed to open file\n");
+        return 1;
+    }
+    char content_buffer[4096];
+    bzero(content_buffer, sizeof(content_buffer));
+    int bytes_read;
+    while((bytes_read = read(file, content_buffer, sizeof(content_buffer))) > 0){
+        if(write(fd, content_buffer, bytes_read) < 0){
+            fprintf(stderr, "failed to write content to client");
+            close(file);
+            return 1;
+        }
+    }
+    
+    
+    close(file);
+    // end copied from part1
+
     return 0;
 }
